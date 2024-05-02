14d706663c168a19545b1b669339ca65e61f5a4f
Oliverx25 <49007548+Oliverx25@users.noreply.github.com>
2024-05-02 15:31:35 -0600

    Component of frecuency

--- Clock Program/Clock_V-1,0.vhd
+++ Clock Program/Clock_V-1,0.vhd
@@ -16,7 +16,23 @@
     -- ========================================================================
     entity Clock is
         Port (
-            
+            -- inputs Switches
+            -- Suplir por un switch, 0 -> Config-Alarm, 1 -> Config-Hour
+            Change_Enable : in STD_LOGIC;                           -- Habilita el cambio de hora
+            Alarm_Enable  : in STD_LOGIC;                           -- Habilita configuración de alarma
+            -- ================================
+            -- inputs Push Buttons
+            Alarm_Save    : in STD_LOGIC;                           -- Guardar alarma
+            Modify_Minute : in STD_LOGIC_VECTOR(5 downto 0);        -- 2^6 > 60 (minutos)
+            Modify_Hour   : in STD_LOGIC_VECTOR(4 downto 0);        -- 2^5 > 24 (horas)
+
+            -- outputs Segments
+            Segments_Hour  : out STD_LOGIC_VECTOR(13 downto 0);     -- 7 segmentos para las horas
+            Segments_Minute: out STD_LOGIC_VECTOR(13 downto 0);     -- 7 segmentos para los minutos
+            Segments_Second: out STD_LOGIC_VECTOR(13 downto 0);     -- 7 segmentos para los segundos
+            -- outputs LEDs
+            LED_Second        : out STD_LOGIC                       -- LED para indicar segundos
+            LED_Alarm_Sequence: out STD_LOGIC_VECTOR(3 downto 0)    -- Secuencia de LEDs para indicar alarma
         );
     end Clock;
 
@@ -24,6 +40,7 @@
     --                       Declaración de la arquitectura
     -- ========================================================================
     architecture Main of Clock is
+    -- Señales a utilizar
     begin
-        
+
     end Main;
